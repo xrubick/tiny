b56f89bc50102fc9de23ff9c68d59c18dcd39950
xcf <xuchangfu@papayamobile.cn>
2021-06-23 18:50:14 +0800

    del comment

--- cmd/root.go
+++ cmd/root.go
@@ -3,45 +3,40 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"time"
+	"context"
+
+	"github.com/vultr/govultr/v2"
+	"golang.org/x/oauth2"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 
 	homedir "github.com/mitchellh/go-homedir"
-	"github.com/spf13/viper"
 )
 
 var cfgFile string
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "tiny",
-	Short: "just a cli tool for vultr cloud",
+	Short: "Tiny is just a cli tool for the vultr api",
 	Long: ``,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	cobra.CheckErr(rootCmd.Execute())
 }
 
 func init() {
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.tiny.yaml)")
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
+	rootCmd.AddCommand(instanceCmd)
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.tiny.yaml)")
 
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
-// initConfig reads in config file and ENV variables if set.
 func initConfig() {
 	if cfgFile != "" {
 		// Use config file from the flag.
